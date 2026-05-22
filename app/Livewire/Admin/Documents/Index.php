c943ad96050949c431001ae59857f7a99e3d0e95
alessandrodallavia <ale.dalla.via@gmail.com>
2026-05-22 15:20:42 +0200

    Modifica order visual documenti

--- app/Livewire/Admin/Documents/Index.php
+++ app/Livewire/Admin/Documents/Index.php
@@ -41,7 +41,7 @@ class Index extends Component
                     });
                 })
                 ->latest('document_date')
-                ->latest('code')
+                ->latest('number')
                 ->latest()
                 ->paginate(15)
                 ->withQueryString(),
